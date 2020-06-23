0b9ff7be1bf021a49489a5759b822e77230d5e66
atsss <atsushi0623itoh@gmail.com>
2020-06-22 22:08:28 -0600

    :tada: model 作成

---
+++ app/models/note.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: notes
+#
+#  id         :bigint           not null, primary key
+#  content    :text(65535)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  place_id   :bigint           not null
+#
+# Indexes
+#
+#  index_notes_on_place_id  (place_id)
+#
+class Note < ApplicationRecord
+  belongs_to :place
+end
---
+++ app/models/place.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: places
+#
+#  id         :bigint           not null, primary key
+#  lat        :float(24)
+#  lon        :float(24)
+#  name       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Place < ApplicationRecord
+  has_many :notes, dependent: :restrict_with_error
+  validates :name, :lat, :lon, presence: true
+end
