01b186cd2ab2beb462987e0626dee0691d2f629a
atsss <atsushi0623itoh@gmail.com>
2020-06-22 21:53:52 -0600

    :tada: places, notes テーブル作成

---
+++ db/migrate/20200623034621_create_places.rb
@@ -0,0 +1,11 @@
+class CreatePlaces < ActiveRecord::Migration[6.0]
+  def change
+    create_table :places do |t|
+      t.string :name
+      t.float :lon
+      t.float :lat
+
+      t.timestamps
+    end
+  end
+end
