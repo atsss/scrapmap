aa3e307d57079086f5f5a73c7714fe62c9ab39b7
atsss <atsushi0623itoh@gmail.com>
2020-07-05 08:44:46 -0600

    :+1: map_tag ヘルパー作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 module ApplicationHelper
+  def map_tag
+    content_tag(:div, style: 'width: 800px;') do
+      content_tag(:div, nil, id: 'map', style: 'width: 800px; height: 400px;')
+    end
+  end
+
   def js_bootstrap_tag
     content_tag(
       :div,
