3e6bfb970bfd909822b9a42ea72ea001701ef119
atsss <atsushi0623itoh@gmail.com>
2020-07-12 00:28:31 -0600

    :tada: Channel モデル作成

---
+++ app/models/channel.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: channels
+#
+#  id         :bigint           not null, primary key
+#  name       :string(255)      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Channel < ApplicationRecord
+  has_many :places, dependent: :restrict_with_error
+  validates :name, presence: true
+end
