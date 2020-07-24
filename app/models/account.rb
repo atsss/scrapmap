61484d8c0eb124b55d8c805080e3da2f786df4bc
atsss <atsushi0623itoh@gmail.com>
2020-07-23 21:58:26 -0600

    :sparkles: Account モデル作成

---
+++ app/models/account.rb
@@ -0,0 +1,29 @@
+# == Schema Information
+#
+# Table name: accounts
+#
+#  id                     :bigint           not null, primary key
+#  current_sign_in_at     :datetime
+#  current_sign_in_ip     :string(255)
+#  email                  :string(255)      default(""), not null
+#  encrypted_password     :string(255)      default(""), not null
+#  last_sign_in_at        :datetime
+#  last_sign_in_ip        :string(255)
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string(255)
+#  sign_in_count          :integer          default(0), not null
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_accounts_on_email                 (email) UNIQUE
+#  index_accounts_on_reset_password_token  (reset_password_token) UNIQUE
+#
+class Account < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :trackable
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :accounts
   root to: 'channels#index'
 
   resources :channels, only: %i(show new create)
