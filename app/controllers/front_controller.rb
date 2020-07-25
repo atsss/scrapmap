2485af2b2a9270b475495b4d9182b4a594db1769
atsss <atsushi0623itoh@gmail.com>
2020-07-24 23:39:31 -0600

    :+1: current_user を front_controller に設定

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -2,4 +2,11 @@ class FrontController < ApplicationController
   layout 'front/application'
 
   before_action :authenticate_account!
+  before_action :set_current_user
+
+  private
+
+  def set_current_user
+    @current_user = current_account.user
+  end
 end
