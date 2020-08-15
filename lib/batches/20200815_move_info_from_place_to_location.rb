c572bb153604219c6a8056af8ac54d4dea65219d
atsss <atsushi0623itoh@gmail.com>
2020-08-15 17:54:22 -0600

    :bug: location 移行バッチの修正

--- lib/batches/20200815_move_info_from_place_to_location.rb
+++ lib/batches/20200815_move_info_from_place_to_location.rb
@@ -3,9 +3,11 @@ require 'batch/base'
 module Batches
   class MoveToInfoFromPlaceToLocation < Batch::Base
     def execute(_args)
-      Place.all.each do |place|
-        location = Location.create!(lat: place.lat, lng: place.lng)
-        place.update!(location: location)
+      Channel.without_deleted.each do |channel|
+        channel.places.each do |place|
+          location = Location.create!(lat: place.lat, lng: place.lng)
+          place.update!(location: location)
+        end
       end
     end
   end
