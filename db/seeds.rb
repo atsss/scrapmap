e1173f269244e0f906e2563572625eabbb230905
atsss <atsushi0623itoh@gmail.com>
2020-08-15 18:13:02 -0600

    :+1: Seed ファイルの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+users_hash = [
+  { name: 'Ats', email: 'test01@example.com', password: 'passpass'},
+  { name: 'Yoshi', email: 'test02@example.com', password: 'passpass'}
+]
+
+users_hash.each { |attributes| Users::Create.run!(attributes) }
+Communities::Create.run!(name: 'CIID', user_ids: User.all.ids)
